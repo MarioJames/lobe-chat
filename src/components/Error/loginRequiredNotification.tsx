63775d0cd521ffa0489c428db4c3504a41cdbb3f
挽歌 <270663716@qq.com>
2026-03-16 21:33:50 +0800

    feat: 优化飞书免登下弹出登录的情况

--- src/components/Error/loginRequiredNotification.tsx
+++ src/components/Error/loginRequiredNotification.tsx
@@ -5,8 +5,15 @@ import { notification } from '@/components/AntdStaticMethods';
 
 import RedirectLogin from './RedirectLogin';
 
+const whiteList = new Set(['/feishu-auto-login']);
+
+const isAutoLoginPage = () =>
+  typeof window !== 'undefined' && whiteList.has(window.location.pathname);
+
 export const loginRequired = {
   redirect: ({ timeout = 2000 }: { timeout?: number } = {}) => {
+    if (isAutoLoginPage()) return;
+
     notification.error({
       description: <RedirectLogin timeout={timeout} />,
       duration: timeout / 1000,
